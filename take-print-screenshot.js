a170c4aec9f692cbe807f53597bde453de41f2e9
Lucas Segurado <lsegurado1996@gmail.com>
2025-07-21 10:36:53 +0200

    Create take-print-screenshot.js

---
+++ take-print-screenshot.js
@@ -0,0 +1,16 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  // Emulate print media
+  await page.emulateMedia({ media: 'screen' });
+
+  // Go to your hosted/local page (use Vercel/Netlify preview or local server)
+  await page.goto(process.env.TARGET_URL);
+
+  await page.screenshot({ path: 'print-screenshot.png', fullPage: true });
+
+  await browser.close();
+})();
