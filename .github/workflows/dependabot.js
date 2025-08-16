a9cf8edd2c0782c58aa5c69faeef1fdb169fab84
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-16 18:26:01 +0200

    Tests

--- .github/workflows/dependabot.js
+++ .github/workflows/dependabot.js
@@ -27,7 +27,7 @@ export default async ({ github, require, params }) => {
       throw 'Not an org package'
     console.log({ org, package_name })
     try {
-      const { data } = await github.rest.getPackageForOrganization({
+      const data = await github.rest.getPackageForOrganization({
         org: org.replace('@', ''),
         package_type: 'npm',
         package_name
@@ -41,7 +41,7 @@ export default async ({ github, require, params }) => {
       };
     }
     catch(e) {
-      console.log(e)
+      console.log({e})
       return {}
     }
   }
