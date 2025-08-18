5c4b26ed3a843475152c3d83612ba2c307581164
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-18 16:27:59 +0200

    Tests

--- .github/actions/dependabot/dependabot.js
+++ .github/actions/dependabot/dependabot.js
@@ -85,7 +85,6 @@ export default async ({ github, require, params }) => {
   const comments = await Promise.all(Object.entries(updatedPackages).map(async ([pkgName, newVersion]) => {
     const [resultPublic, result] = await Promise.allSettled([getPublicRepoInfo(pkgName), getRepoInfo(pkgName)]);
     const [owner, repo] = [resultPublic.value?.owner ?? result.value?.owner, resultPublic.value?.repo ?? result.value?.repo];
-    console.log({ pkgName, owner, repo })
     if (!owner || !repo) return;
 
     const oldVersion =
