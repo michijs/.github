dc92658e4297ffe1e283ad9f2ceef39a9425116e
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-20 16:33:50 +0200

    Tests

--- .github/actions/dependabot/main.js
+++ .github/actions/dependabot/main.js
@@ -48,7 +48,7 @@ export default async ({ github, require, params }) => {
   }
 
   function clearBody(body){
-    return body.replaceAll('@', '@&ZeroWidthSpace;').replaceAll('github.com', 'redirect.github.com') + '<a href="https://github.com/dayanaramirez601">@dayanaramirez601</a>'
+    return body.replaceAll('@', '@&ZeroWidthSpace;').replaceAll('github.com', 'redirect.github.com') + '   @dayanaramirez601'
   }
 
   async function getChangelog(owner, repo, oldVersion) {
