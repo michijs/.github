87b83131f8e76dd871d12acf116e05e8ff90ca90
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-30 16:38:24 +0200

    Test

--- .github/scripts/run.ts
+++ .github/scripts/run.ts
@@ -1,35 +1,32 @@
 import { $ } from "bun";
-import type { Command, Params } from "./types";
+import type { Command, RunParams, WorkflowParams } from "./types";
 
-function normalizeCommands(command: Params['command']): Command[] {
+function normalizeCommands(command: RunParams['command']): Command[] {
   // Normalize to array of {name, script}
   if (typeof command === "string") {
     if (command === '')
       throw 'Script is empty'
-      return [{ name: command, script: command }];
+    return [{ name: command, script: command }];
   } else
     return command;
 }
 
-export default async ({ params }: {params: Params}) => {
+export default async function run({ params, runGroup }: WorkflowParams<RunParams>) {
   try {
     const commands = normalizeCommands(params.command);
     let error;
     // Run all commands in parallel
     const result = await Promise.allSettled(
       commands.map(async ({ name, script }) => {
-        try {
-          const result = await $`bun run ${script}`;
-          console.log(`::group::✅ ${name}`);
-          console.log(result.text());
-          console.log(`::endgroup::`);
-        } catch (err) {
-          console.log(`::group::⛔ ${name}`);
-          error = err;
-          console.log(`::error::${err}`);
-          console.log(`::endgroup::`);
-          throw err;
-        }
+        runGroup(name, async () => {
+          try {
+            const result = await $`bun run ${script}`;
+            return result.text()
+          } catch (err) {
+            error = err;
+            throw err;
+          }
+        })
       })
     );
     if (error)
--- .github/scripts/types.d.ts
+++ .github/scripts/types.d.ts
@@ -3,6 +3,15 @@ export interface Command {
   name: string,
   script: string
 }
-export interface Params {
+export interface RunParams {
   command: string | Command[]
+}
+
+export interface Workflow<P> {
+  (p: WorkflowParams<P>)
+}
+export interface WorkflowParams<P> {
+  params: P,
+  importScriptFromUrl<V extends Workflow<any>>(url: string): V,
+  runGroup(name: string, callback: () => any | Promise<any>)
 }
